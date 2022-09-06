8f318c50cc2bde5fb3e05b635f0f1c104cf531e1
ShunchaoChen <shunchao.chen@klaviyo.com>
2022-09-06 19:09:40 -0400

    activated 7 days added

--- Dash Cohort Queries.sql
+++ Dash Cohort Queries.sql
@@ -1,6 +1,7 @@
-------- Combination: Overall / URL Brkdwn / Channel Brkdwn / 
+------- Combination: Overall / URL Brkdwn / Channel Brkdwn / device Brkdwn / Country Brkdwn / New & Return Brkdwn
 
--------Geo Info CTE --------------
+
+------- Geo Info CTE --------------
 with country_ref as (
 select 
 country
@@ -8,6 +9,51 @@ country
 from klaviyo.sandbox.country_mapping
 group by 1,2
 )
+-------- Activated -----------------
+, activated_base as (
+-- activated actions combined
+    
+-- publish flow
+  select current_company_id as aid, convert_timezone('America/Los_Angeles', 'UTC', min(time)) as first
+  from heap_main_production.heap.set_flow_email_to_live
+  group by 1
+
+union
+-- publish form
+  select current_company_id as aid, convert_timezone('America/Los_Angeles', 'UTC', min(time)) as first
+  from heap_main_production.heap.published_form
+  group by 1
+  
+union
+-- send or schedule campaign 
+  select current_company_id as aid, convert_timezone('America/Los_Angeles', 'UTC', min(time)) as first
+  from heap_main_production.heap.scheduled_campaign
+  group by 1
+  
+), activated as (
+-- earliest activated date by klaviyo account id    
+    select aid as klaviyo_account_id
+    , min(date_trunc('day', first)) as min_activated_date
+    from activated_base 
+    group by 1
+    order by 1
+    
+), base_join as (
+-- join bi_lead_id_mapping for dedup        
+    select distinct 
+        bil.* 
+    , activated.min_activated_date       
+    from klaviyo.sandbox.bi_lead_id_mapping bil    
+     left join activated on activated.klaviyo_account_id = bil.klaviyo_account_id   
+    
+), lead_aggregation as (    
+-- aggregate to bi_lead_id level (alrdy confirmed min('2021-01-02', null)='2021-01-02')
+    select b.bi_lead_id
+    , min(min_activated_date) as min_activated_date   
+    from base_join b
+    group by b.bi_lead_id
+)
+
 
 SELECT *
 FROM
@@ -27,6 +73,7 @@ FROM
     ,count(distinct case when af.lead_mql_date is not null and af.mql_flag = 1 and af.klaviyo_account_id is not null then af.klaviyo_account_id else null end) as mqls
     ,count(distinct case when af.klaviyo_account_created_date is not null and af.klaviyo_account_id is not null then af.klaviyo_account_id else null end) as signups
     ,count(distinct case when af.demo_request_date is not null and af.klaviyo_account_id is not null then af.klaviyo_account_id else null end) as demo_requests
+    ,count(distinct case when datediff(day,date_trunc('day', la.min_activated_date)::date,h.visit_start_time) between 0 and 6 then af.klaviyo_account_id else NULL end) AS Activated_7D
     , round(div0(mqls, visits),4)*100 as MQL_CVR
     , round(div0(signups,visits),4)*100 as Signup_CVR
     , round(div0(demo_requests,visits),4)*100 as DemoRequests_CVR
@@ -34,6 +81,7 @@ FROM
     LEFT JOIN public.TBL_GA_SESSIONS ga on h.full_visitor_id = ga.full_visitor_id and h.visit_id = ga.visit_id
     LEFT JOIN public.tbl_account_dimensions ad on ga.klaviyo_account_id = ad.klaviyo_account_id
     LEFT JOIN klaviyo.sandbox.tbl_acquisition_funnel af on ga.klaviyo_account_id = af.klaviyo_account_id
+    LEFT JOIN lead_aggregation la on af.bi_lead_id = la.bi_lead_id
     where split_part(h.h__value:page.pagePath::string,'?',0)
     in (
     '/',
@@ -67,6 +115,7 @@ FROM
     ,count(distinct case when af.lead_mql_date is not null and af.mql_flag = 1 and af.klaviyo_account_id is not null then af.klaviyo_account_id else null end) as mqls
     ,count(distinct case when af.klaviyo_account_created_date is not null and af.klaviyo_account_id is not null then af.klaviyo_account_id else null end) as signups
     ,count(distinct case when af.demo_request_date is not null and af.klaviyo_account_id is not null then af.klaviyo_account_id else null end) as demo_requests
+    ,count(distinct case when datediff(day,date_trunc('day', la.min_activated_date)::date,h.visit_start_time) between 0 and 6 then af.klaviyo_account_id else NULL end) AS Activated_7D
     , round(div0(mqls, visits),4)*100 as MQL_CVR
     , round(div0(signups,visits),4)*100 as Signup_CVR
     , round(div0(demo_requests,visits),4)*100 as DemoRequests_CVR
@@ -74,6 +123,7 @@ FROM
     LEFT JOIN public.TBL_GA_SESSIONS ga on h.full_visitor_id = ga.full_visitor_id and h.visit_id = ga.visit_id
     LEFT JOIN public.tbl_account_dimensions ad on ga.klaviyo_account_id = ad.klaviyo_account_id
     LEFT JOIN klaviyo.sandbox.tbl_acquisition_funnel af on ga.klaviyo_account_id = af.klaviyo_account_id
+    LEFT JOIN lead_aggregation la on af.bi_lead_id = la.bi_lead_id
     where split_part(h.h__value:page.pagePath::string,'?',0)
     in (
     '/',
@@ -107,6 +157,7 @@ FROM
     ,count(distinct case when af.lead_mql_date is not null and af.mql_flag = 1 and af.klaviyo_account_id is not null then af.klaviyo_account_id else null end) as mqls
     ,count(distinct case when af.klaviyo_account_created_date is not null and af.klaviyo_account_id is not null then af.klaviyo_account_id else null end) as signups
     ,count(distinct case when af.demo_request_date is not null and af.klaviyo_account_id is not null then af.klaviyo_account_id else null end) as demo_requests
+    ,count(distinct case when datediff(day,date_trunc('day', la.min_activated_date)::date,h.visit_start_time) between 0 and 6 then af.klaviyo_account_id else NULL end) AS Activated_7D
     , round(div0(mqls, visits),4)*100 as MQL_CVR
     , round(div0(signups,visits),4)*100 as Signup_CVR
     , round(div0(demo_requests,visits),4)*100 as DemoRequests_CVR
@@ -114,6 +165,7 @@ FROM
     LEFT JOIN public.TBL_GA_SESSIONS ga on h.full_visitor_id = ga.full_visitor_id and h.visit_id = ga.visit_id
     LEFT JOIN public.tbl_account_dimensions ad on ga.klaviyo_account_id = ad.klaviyo_account_id
     LEFT JOIN klaviyo.sandbox.tbl_acquisition_funnel af on ga.klaviyo_account_id = af.klaviyo_account_id
+    LEFT JOIN lead_aggregation la on af.bi_lead_id = la.bi_lead_id
     where split_part(h.h__value:page.pagePath::string,'?',0)
     in (
     '/',
@@ -147,6 +199,7 @@ FROM
     ,count(distinct case when af.lead_mql_date is not null and af.mql_flag = 1 and af.klaviyo_account_id is not null then af.klaviyo_account_id else null end) as mqls
     ,count(distinct case when af.klaviyo_account_created_date is not null and af.klaviyo_account_id is not null then af.klaviyo_account_id else null end) as signups
     ,count(distinct case when af.demo_request_date is not null and af.klaviyo_account_id is not null then af.klaviyo_account_id else null end) as demo_requests
+    ,count(distinct case when datediff(day,date_trunc('day', la.min_activated_date)::date,h.visit_start_time) between 0 and 6 then af.klaviyo_account_id else NULL end) AS Activated_7D
     , round(div0(mqls, visits),4)*100 as MQL_CVR
     , round(div0(signups,visits),4)*100 as Signup_CVR
     , round(div0(demo_requests,visits),4)*100 as DemoRequests_CVR
@@ -154,6 +207,7 @@ FROM
     LEFT JOIN public.TBL_GA_SESSIONS ga on h.full_visitor_id = ga.full_visitor_id and h.visit_id = ga.visit_id
     LEFT JOIN public.tbl_account_dimensions ad on ga.klaviyo_account_id = ad.klaviyo_account_id
     LEFT JOIN klaviyo.sandbox.tbl_acquisition_funnel af on ga.klaviyo_account_id = af.klaviyo_account_id
+    LEFT JOIN lead_aggregation la on af.bi_lead_id = la.bi_lead_id
     where split_part(h.h__value:page.pagePath::string,'?',0)
     in (
     '/',
@@ -188,6 +242,7 @@ FROM
     ,count(distinct case when af.lead_mql_date is not null and af.mql_flag = 1 and af.klaviyo_account_id is not null then af.klaviyo_account_id else null end) as mqls
     ,count(distinct case when af.klaviyo_account_created_date is not null and af.klaviyo_account_id is not null then af.klaviyo_account_id else null end) as signups
     ,count(distinct case when af.demo_request_date is not null and af.klaviyo_account_id is not null then af.klaviyo_account_id else null end) as demo_requests
+    ,count(distinct case when datediff(day,date_trunc('day', la.min_activated_date)::date,h.visit_start_time) between 0 and 6 then af.klaviyo_account_id else NULL end) AS Activated_7D
     , round(div0(mqls, visits),4)*100 as MQL_CVR
     , round(div0(signups,visits),4)*100 as Signup_CVR
     , round(div0(demo_requests,visits),4)*100 as DemoRequests_CVR
@@ -195,6 +250,7 @@ FROM
     LEFT JOIN public.TBL_GA_SESSIONS ga on h.full_visitor_id = ga.full_visitor_id and h.visit_id = ga.visit_id
     LEFT JOIN public.tbl_account_dimensions ad on ga.klaviyo_account_id = ad.klaviyo_account_id
     LEFT JOIN klaviyo.sandbox.tbl_acquisition_funnel af on ga.klaviyo_account_id = af.klaviyo_account_id
+    LEFT JOIN lead_aggregation la on af.bi_lead_id = la.bi_lead_id
     LEFT JOIN country_ref cf on cf.country = trim(ga.GEO_NETWORK['country'],'"')
     where split_part(h.h__value:page.pagePath::string,'?',0)
     in (
@@ -234,6 +290,7 @@ FROM
     ,count(distinct case when af.lead_mql_date is not null and af.mql_flag = 1 and af.klaviyo_account_id is not null then af.klaviyo_account_id else null end) as mqls
     ,count(distinct case when af.klaviyo_account_created_date is not null and af.klaviyo_account_id is not null then af.klaviyo_account_id else null end) as signups
     ,count(distinct case when af.demo_request_date is not null and af.klaviyo_account_id is not null then af.klaviyo_account_id else null end) as demo_requests
+    ,count(distinct case when datediff(day,date_trunc('day', la.min_activated_date)::date,h.visit_start_time) between 0 and 6 then af.klaviyo_account_id else NULL end) AS Activated_7D
     , round(div0(mqls, visits),4)*100 as MQL_CVR
     , round(div0(signups,visits),4)*100 as Signup_CVR
     , round(div0(demo_requests,visits),4)*100 as DemoRequests_CVR
@@ -241,6 +298,7 @@ FROM
     LEFT JOIN public.TBL_GA_SESSIONS ga on h.full_visitor_id = ga.full_visitor_id and h.visit_id = ga.visit_id
     LEFT JOIN public.tbl_account_dimensions ad on ga.klaviyo_account_id = ad.klaviyo_account_id
     LEFT JOIN klaviyo.sandbox.tbl_acquisition_funnel af on ga.klaviyo_account_id = af.klaviyo_account_id
+    LEFT JOIN lead_aggregation la on af.bi_lead_id = la.bi_lead_id
     where split_part(h.h__value:page.pagePath::string,'?',0)
     in (
     '/',
